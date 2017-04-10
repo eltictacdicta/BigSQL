3ba11984c541cbf32adbe69ba562d6c1b05fec68
Francisco Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2017-04-10 10:07:55 +0200

    Revertidos algunos cambios.

--- bigdump.php
+++ bigdump.php
@@ -913,6 +913,39 @@ function create_ajax_script()
   global $linenumber, $foffset, $totalqueries, $delaypersession, $curfilename, $delimiter;
 	echo '<script type="text/javascript" language="javascript">			
 	
+	
+	// extracts text from XML element (itemname must be unique)
+	function get_xml_data(itemname,xmld) {
+		return xmld.getElementsByTagName(itemname).item(0).firstChild.data;
+	}
+	
+	function makeRequest(url) {
+		http_request = false;
+		if (window.XMLHttpRequest) { 
+		// Mozilla etc.
+			http_request = new XMLHttpRequest();
+			if (http_request.overrideMimeType) {
+				http_request.overrideMimeType("text/xml");
+			}
+		} else if (window.ActiveXObject) { 
+		// IE
+			try {
+				http_request = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				try {
+					http_request = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch(e) {}
+			}
+		}
+		if (!http_request) {
+				alert("Cannot create an XMLHTTP instance");
+				return false;
+		}
+		http_request.onreadystatechange = server_response;
+		http_request.open("GET", url, true);
+		http_request.send(null);
+	}
+	
 	function server_response() 
 	{
 
