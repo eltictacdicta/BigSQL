67867cacc7f5f38986c92b56881dbe5c72151093
Francisco Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2017-04-10 10:47:46 +0200

    Creado un index y añadido un archivo config para las variables y constantes de configuración.

--- bigdump.php
+++ bigdump.php
@@ -37,64 +37,13 @@ error_reporting(E_ALL);
 // *** Change from mySQL to mySQLi
 
 // Database configuration
+defined( 'DB_SERVER' ) or die( ':(' );
+$db_server   = DB_SERVER;
+$db_name     = DB_NAME;
+$db_username = DB_USERNAME;
+$db_password = DB_PASSWORD;
 
-$db_server   = 'localhost';
-$db_name     = 'prestashopimporter';
-$db_username = 'root';
-$db_password = ''; 
 
-// Connection charset should be the same as the dump file charset (utf8, latin1, cp1251, koi8r etc.)
-// See http://dev.mysql.com/doc/refman/5.0/en/charset-charsets.html for the full list
-// Change this if you have problems with non-latin letters
-
-$db_connection_charset = 'utf8';
-
-// OPTIONAL SETTINGS 
-
-$filename           = '';     // Specify the dump filename to suppress the file selection dialog
-$ajax               = true;   // AJAX mode: import will be done without refreshing the website
-$linespersession    = 30000;   // Lines to be executed per one import session
-$delaypersession    = 0;      // You can specify a sleep time in milliseconds after each session
-                              // Works only if JavaScript is activated. Use to reduce server overrun
-
-// CSV related settings (only if you use a CSV dump)
-
-$csv_insert_table   = '';     // Destination table for CSV files
-$csv_preempty_table = false;  // true: delete all entries from table specified in $csv_insert_table before processing
-$csv_delimiter      = ',';    // Field delimiter in CSV file
-$csv_add_quotes     = true;   // If your CSV data already have quotes around each field set it to false
-$csv_add_slashes    = true;   // If your CSV data already have slashes in front of ' and " set it to false
-
-// Allowed comment markers: lines starting with these strings will be ignored by BigDump
-
-$comment[]='#';                       // Standard comment lines are dropped by default
-$comment[]='-- ';
-$comment[]='DELIMITER';               // Ignore DELIMITER switch as it's not a valid SQL statement
-// $comment[]='---';                  // Uncomment this line if using proprietary dump created by outdated mysqldump
-// $comment[]='CREATE DATABASE';      // Uncomment this line if your dump contains create database queries in order to ignore them
-$comment[]='/*!';                     // Or add your own string to leave out other proprietary things
-
-// Pre-queries: SQL queries to be executed at the beginning of each import session
-
-// $pre_query[]='SET foreign_key_checks = 0';
-// $pre_query[]='Add additional queries if you want here';
-
-// Default query delimiter: this character at the line end tells Bigdump where a SQL statement ends
-// Can be changed by DELIMITER statement in the dump file (normally used when defining procedures/functions)
-
-$delimiter = ';';
-
-// String quotes character
-
-$string_quotes = '\'';                  // Change to '"' if your dump file uses double qoutes for strings
-
-// How many lines may be considered to be one query (except text lines)
-
-$max_query_lines = 5000;
-
-// Where to put the upload files into (default: bigdump folder)
-
-$upload_dir = dirname(__FILE__);
 
 // *******************************************************************************************
 // If not familiar with PHP please don't change anything below this line
